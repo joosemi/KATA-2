f828a8ca067d87e8a4cc6346d372edf0eda586e7
usuario <usuario@10.22.143.9>
2015-09-30 10:00:32 +0100

    Versión 2

--- src/histograma/Histogram.java
+++ src/histograma/Histogram.java
@@ -2,19 +2,15 @@ package histograma;
 
 import java.util.HashMap;
 
-public class Histogram {
-    private final int[] array;
+public class Histogram <T> {
+    private final T[] array;
 
-    public Histogram(int[] array) {
+    public Histogram(T[] array) {
         this.array = array;
     }
-
-    public int[] getArray() {
-        return array;
-    }
     
-    public HashMap<Integer, Integer> getHistogram() {
-        HashMap<Integer, Integer> histogram = new HashMap<>();
+    public HashMap<T, Integer> getHistogram() {
+        HashMap<T, Integer> histogram = new HashMap<>();
         for (int i = 0; i < array.length; i++) {
             if(!histogram.containsKey(array[i])) {
                 histogram.put(array[i],0);
